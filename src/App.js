53ff4d7f332eb83e9907d4f84383397b6b0beeda
SokoPaulSokool <sopapaso73@gmail.com>
2020-02-19 16:31:59 +0300

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch,  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
-import LandingPage from "./LandingPage/LandingPage";
-import BorrowerPage from "./BorrowerPage/BorrowerPage";
+import LandingPage from './LandingPage/LandingPage';
+import BorrowerPage from './BorrowerPage/BorrowerPage';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <Router >
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/borrow" component={BorrowerPage} />
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/borrow" component={BorrowerPage} />
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
